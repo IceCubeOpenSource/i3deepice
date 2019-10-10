012983fbd43865d9d0d6713ed132596107cbadfa
tglauch <theo.glauch@tum.de>
2019-10-10 15:10:58 +0200

    not yet tested for tensorflow 2.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
         ],
     python_requires=">=2.7",
     install_requires=[
-        'tensorflow>=1.5, 2.0',
+        'tensorflow>=1.5,<2.0',
         'keras>=2.0',
         'numpy>1.14'],
     packages = setuptools.find_packages(),
